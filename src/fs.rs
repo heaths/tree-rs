64c4e61f5a98de7930d3954a6ce9438e0a970050
Heath Stewart <heaths@outlook.com>
2024-08-17 15:39:30 -0700

    Add more fs module tests (#2)

--- src/fs.rs
+++ src/fs.rs
@@ -34,7 +34,7 @@ impl FileSystemNode {
 impl fmt::Debug for FileSystemNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("FileSystemNode")
-            .field("entry", &self.entry.path())
+            .field("entry", &self.to_string())
             .finish()
     }
 }
@@ -43,9 +43,9 @@ impl fmt::Display for FileSystemNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let path = self.entry.path();
         let s = if f.alternate() {
-            path.file_name().ok_or(fmt::Error)?
-        } else {
             path.as_os_str()
+        } else {
+            path.file_name().ok_or(fmt::Error)?
         };
         let s = s.to_str().ok_or(fmt::Error)?;
         if path.is_dir() {
@@ -105,17 +105,16 @@ impl Iterator for NodeIterator {
 
 #[cfg(test)]
 mod tests {
-    use std::fs::read_dir;
-
     use super::*;
+    use std::{ffi::OsStr, fs::read_dir, path::Path};
 
     #[test]
     fn has_children() {
         let mut found_dir = false;
         let mut found_file = false;
 
-        let dir = NodeIterator(read_dir(env!("CARGO_MANIFEST_DIR")));
-        for node in dir {
+        let iter = NodeIterator(read_dir(env!("CARGO_MANIFEST_DIR")));
+        for node in iter {
             if node.path().is_dir() {
                 found_dir = true;
                 assert!(node.to_string().ends_with("/"));
@@ -128,5 +127,77 @@ mod tests {
 
         assert!(found_dir);
         assert!(found_file);
+    }
+
+    #[test]
+    fn display() {
+        let nodes = FileSystemNodes::new().unwrap();
+
+        assert_eq!("src/", &nodes.src_dir.to_string());
+        assert_eq!(
+            Path::new(env!("CARGO_MANIFEST_DIR"))
+                .join("src/")
+                .display()
+                .to_string(),
+            format!("{:#}", &nodes.src_dir)
+        );
+
+        assert_eq!("Cargo.toml", &nodes.manifest_file.to_string());
+        assert_eq!(
+            Path::new(env!("CARGO_MANIFEST_DIR"))
+                .join("Cargo.toml")
+                .display()
+                .to_string(),
+            format!("{:#}", &nodes.manifest_file),
+        );
+    }
+
+    #[test]
+    fn debug() {
+        let nodes = FileSystemNodes::new().unwrap();
+
+        assert_eq!(
+            "FileSystemNode { entry: \"src/\" }",
+            &format!("{:?}", &nodes.src_dir)
+        );
+        assert_eq!(
+            "FileSystemNode { entry: \"Cargo.toml\" }",
+            &format!("{:?}", &nodes.manifest_file)
+        );
+    }
+
+    struct FileSystemNodes {
+        src_dir: FileSystemNode,
+        manifest_file: FileSystemNode,
+    }
+
+    impl FileSystemNodes {
+        fn new() -> io::Result<Self> {
+            let mut src_dir: Option<FileSystemNode> = None;
+            let mut manifest_file: Option<FileSystemNode> = None;
+
+            let iter = NodeIterator(read_dir(env!("CARGO_MANIFEST_DIR")));
+            for entry in iter {
+                if entry.is_dir()? && entry.path().file_name() == Some(OsStr::new("src")) {
+                    src_dir = Some(entry);
+                } else if entry.is_file()?
+                    && entry.path().file_name() == Some(OsStr::new("Cargo.toml"))
+                {
+                    manifest_file = Some(entry);
+                }
+            }
+
+            if let (Some(src_dir), Some(manifest_file)) = (src_dir, manifest_file) {
+                return Ok(Self {
+                    src_dir,
+                    manifest_file,
+                });
+            }
+
+            Err(io::Error::new(
+                io::ErrorKind::NotFound,
+                "src/ or Cargo.toml not found",
+            ))
+        }
     }
 }
